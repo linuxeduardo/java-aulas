0824aec6e3506f838989768afaa2090bfabba5d4
eduardo239 <eucrieiumaconta@gmail.com>
2021-08-18 20:38:29 -0300

    forked

---
+++ src/oo/composicao/desafio/Cliente.java
@@ -0,0 +1,27 @@
+package oo.composicao.desafio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+    final String nome;
+    final List<Compra> compras = new ArrayList<>();
+
+    Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    void adicionarCompra(Compra c) {
+        this.compras.add(c);
+    }
+
+    double obterValorTotal() {
+        double total = 0;
+        System.out.println(this.compras.get(0).items);
+        for(Compra c: compras) {
+            total += c.obterValorTotal();
+        }
+        return 0;
+    }
+
+}
