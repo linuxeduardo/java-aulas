c705432e3b292836591ed681e8ec0d2e45cdacd6
linuxeduardo <eduardo.ar.85@gmail.com>
2021-08-19 16:30:58 -0300

    encapsulamento

--- src/oo/composicao/Carro.java
+++ src/oo/composicao/Carro.java
@@ -5,27 +5,27 @@ public class Carro {
 	// 1 car 1 motor
 	Motor motor = new Motor();
 
-	void acelerar() {
-		if (motor.fatorInjecao < 2.5) {
-			motor.fatorInjecao += 0.4;
-		}
-	}
-
-	void frear() {
-		if (motor.fatorInjecao > 0.5) {
-			motor.fatorInjecao -= 0.4;
-		}
-	}
-
-	void ligar() {
-		motor.ligado = true;
-	}
-
-	void desligar() {
-		motor.ligado = false;
-	}
-
-	boolean estaLigado() {
-		return motor.ligado;
-	}
+//	void acelerar() {
+//		if (motor.fatorInjecao < 2.5) {
+//			motor.fatorInjecao += 0.4;
+//		}
+//	}
+//
+//	void frear() {
+//		if (motor.fatorInjecao > 0.5) {
+//			motor.fatorInjecao -= 0.4;
+//		}
+//	}
+//
+//	void ligar() {
+//		motor.ligado = true;
+//	}
+//
+//	void desligar() {
+//		motor.ligado = false;
+//	}
+//
+//	boolean estaLigado() {
+//		return motor.ligado;
+//	}
 }
---
+++ src/oo/composicao/encapsulamento/casaA/Ana.java
@@ -0,0 +1,9 @@
+package oo.composicao.encapsulamento.casaA;
+
+public class Ana {
+	@SuppressWarnings("unused")
+	private String segredo = "...";
+	String dentroDeCasa = "...";
+	protected String formaDeFalar = "...";
+	public String todosSabem = "...";
+}
